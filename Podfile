001bdaa2fae5a6d9fcb80edbecbb0cae821d173e
Jorge Luis Perales <jorge.perales@enviaflores.com>
2019-09-28 14:38:49 -0500

    Add onboarding experience
    SwiftyOnboard pod facilitates the creation of onboarding experiences,

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'restaurant-app' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for restaurant-app
+ pod 'SwiftyJSON'
+ pod 'AlamofireImage'
+ pod 'SwiftyOnboard'
+end
---
+++ restaurant-app/Utils/SessionManager.swift
@@ -0,0 +1,30 @@
+//
+//  SessionManager.swift
+//  restaurant-app
+//
+//  Created by Jorge Luis Perales on 9/28/19.
+//  Copyright © 2019 Jorge Luis Perales. All rights reserved.
+//
+
+import Foundation
+
+final class SessionManager {
+  
+  private static let userDefaults = UserDefaults.standard
+  
+  static func clear() {
+    let dictionary = userDefaults.dictionaryRepresentation()
+    for (key, _) in dictionary {
+      self.userDefaults.removeObject(forKey: key)
+    }
+    userDefaults.synchronize()
+  }
+  
+  static func setUserCompletedOnboard(_ completed: Bool) {
+    self.userDefaults.set(completed, forKey: "completed_onboard")
+  }
+  
+  static func getUserCompletedOnboard() -> Bool {
+    return self.userDefaults.bool(forKey: "completed_onboard")
+  }
+}
